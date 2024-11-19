9d876574ca976fc67394455f77b0eeda1eb58e32
RainLasea <1178391318@qq.com>
2024-11-19 21:21:46 +0800

    Optimized code judgment

--- overrides/kubejs/server_scripts/TFCRE_toolrecipes.js
+++ overrides/kubejs/server_scripts/TFCRE_toolrecipes.js
@@ -1,114 +1,37 @@
 ServerEvents.loaded(event =>{
     event.server.scheduleInTicks(1,() =>{})
-})
+});
 //石
 ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == 'tfc:stone/axe_head/igneous_extrusive' && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
+    let MainHandItem = event.player.mainHandItem;
+    let offHandItem = event.player.offHandItem;
+    let aex_stoneMatch = /tfc:stone\/axe_head\/(.*)/.exec(MainHandItem.id);
+    let aex_stone = aex_stoneMatch[1];
+    if(MainHandItem === (Item.of(`tfc:stone/axe_head/${aex_stone}`)) && offHandItem === 'minecraft:stick') {
         if(event.hand != "MAIN_HAND")return;
         event.player.triggerAnimation("tfcre:crafttool");
-        //计时器
-        event.server.scheduleInTicks(20*1.21,() =>{
+        event.server.scheduleInTicks(20 * 1.21, () => {
             offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand('tfc:stone/axe/igneous_extrusive');
-        })
+            MainHandItem.count--;
+            event.player.giveInHand(Item.of(`tfc:stone/axe/${aex_stone}`, '{Damage:0}'));
+        });
     }
-})
+});
 ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == 'tfc:stone/axe_head/igneous_intrusive' && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
+    let MainHandItem = event.player.mainHandItem;
+    let offHandItem = event.player.offHandItem;
+    let ItemNbt = event.player.mainHandItem.nbt;
+    let forgingBonusMatch = /"tfc:forging_bonus":(\d+)/.exec(ItemNbt);
+    let forgingBonus = forgingBonusMatch ? forgingBonusMatch[1] : 0; // 如果没有找到，默认值为 0
+    if (MainHandItem === Item.of(`tfc:metal/axe_head/copper`,`{Damage:0,"tfc:forging_bonus":${forgingBonus}}`) && event.player.offHandItem === 'minecraft:stick') {
         if(event.hand != "MAIN_HAND")return;
         event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
+        event.player.server.scheduleInTicks(20 * 1.21, () => {
             offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:stone/axe/igneous_intrusive', '{Damage:0}'));
-        })
+            MainHandItem.count--;
+            event.player.giveInHand(Item.of(`tfc:metal/axe/copper`, `{Damage:0,"tfc:forging_bonus":${forgingBonus}}`));
+        });
     }
 })
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == 'tfc:stone/axe_head/metamorphic' && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:stone/axe/metamorphic', '{Damage:0}'));
-        })
-    }
-})
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == 'tfc:stone/axe_head/sedimentary' && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:stone/axe/sedimentary', '{Damage:0}'));
-        })
-    }
-})
-//铜
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == 'tfc:metal/axe_head/copper' && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:metal/axe/copper', '{Damage:0}'));
-        })
-    }
-})
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == (Item.of('tfc:metal/axe_head/bronze', '{"tfc:forging_bonus":1}')) && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:metal/axe/bronze', '{Damage:0,"tfc:forging_bonus":1}'));
-        })
-    }
-})
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == (Item.of('tfc:metal/axe_head/bronze', '{"tfc:forging_bonus":2}')) && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:metal/axe/bronze', '{Damage:0,"tfc:forging_bonus":2}'));
-        })
-    }
-})
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == (Item.of('tfc:metal/axe_head/bronze', '{"tfc:forging_bonus":3}')) && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:metal/axe/bronze', '{Damage:0,"tfc:forging_bonus":3}'));
-        })
-    }
-})
-ItemEvents.firstRightClicked(event => {
-    if (event.player.mainHandItem == (Item.of('tfc:metal/axe_head/bronze', '{"tfc:forging_bonus":4}')) && event.player.offHandItem == 'minecraft:stick') {
-        let offHandItem = event.player.offHandItem;
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20*1.21,() =>{
-            offHandItem.count--;
-            event.item.count--;
-            event.player.giveInHand(Item.of('tfc:metal/axe/bronze', '{Damage:0,"tfc:forging_bonus":4}'));
-        })
-    }
-})
+
+
