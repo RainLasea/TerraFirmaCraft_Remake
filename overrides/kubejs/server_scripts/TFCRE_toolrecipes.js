0100ecf0f934f690ce5d2693ec8f5d11bae7088b
RainLasea <1178391318@qq.com>
2024-11-20 13:18:50 +0800

    Update ToolRecipes

--- overrides/kubejs/server_scripts/TFCRE_toolrecipes.js
+++ overrides/kubejs/server_scripts/TFCRE_toolrecipes.js
@@ -5,78 +5,25 @@ ItemEvents.firstRightClicked(event => {
     let MainHandItem = event.player.mainHandItem;
     let offHandItem = event.player.offHandItem;
 
-    let StoneMatch = /tfc:stone\/(?:axe_head|hammer_head|hoe_head|javelin_head|knife_head|shovel_head)\/([^/]+)/.exec(MainHandItem.id);
-    let Stone = StoneMatch ? StoneMatch[1] : null;// 如果没有找到，默认值为 null
+    if (MainHandItem.isEmpty()) return; // 如果主手为空，则直接返回
 
+    //锻造品质
     let forgingBonusMatch = /"tfc:forging_bonus":(\d+)/.exec(MainHandItem.nbt);
     let forgingBonus = forgingBonusMatch ? forgingBonusMatch[1] : 0;
-    //石斧
-    if(MainHandItem === (Item.of(`tfc:stone/axe_head/${Stone}`)) && offHandItem === 'minecraft:stick') {
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20 * 1.21, () => {
-            offHandItem.count--;
-            MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:stone/axe/${Stone}`, '{Damage:0}'));
-        });
-    };
-    //石锤
-    if(MainHandItem === (Item.of(`tfc:stone/hammer_head/${Stone}`)) && offHandItem === 'minecraft:stick') {
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20 * 1.21, () => {
-            offHandItem.count--;
-            MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:stone/hammer/${Stone}`, '{Damage:0}'));
-        });
-    };
-    //石锄
-    if(MainHandItem === (Item.of(`tfc:stone/hoe_head/${Stone}`)) && offHandItem === 'minecraft:stick') {
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20 * 1.21, () => {
-            offHandItem.count--;
-            MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:stone/hoe/${Stone}`, '{Damage:0}'));
-        });
-    };
-    //石标枪
-    if(MainHandItem === (Item.of(`tfc:stone/javelin_head/${Stone}`)) && offHandItem === 'minecraft:stick') {
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20 * 1.21, () => {
-            offHandItem.count--;
-            MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:stone/javelin/${Stone}`, '{Damage:0}'));
-        });
-    };
-    //石刀
-    if(MainHandItem === (Item.of(`tfc:stone/knife_headn/${Stone}`)) && offHandItem === 'minecraft:stick') {
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20 * 1.21, () => {
-            offHandItem.count--;
-            MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:stone/knife/${Stone}`, '{Damage:0}'));
-        });
-    };
-    //石铲
-    if(MainHandItem === (Item.of(`tfc:stone/shovel_headn/${Stone}`)) && offHandItem === 'minecraft:stick') {
-        if(event.hand != "MAIN_HAND")return;
-        event.player.triggerAnimation("tfcre:crafttool");
-        event.server.scheduleInTicks(20 * 1.21, () => {
-            offHandItem.count--;
-            MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:stone/shovel/${Stone}`, '{Damage:0}'));
-        });
-    };
-    if (MainHandItem === (Item.of(`tfc:metal/axe_head/copper`,`{Damage:0,"tfc:forging_bonus":${forgingBonus}}`)) && event.player.offHandItem === 'minecraft:stick') {
+    //工具变种
+    let ToolTypesMatch = /tfc:([a-z_]+)+\/([a-z_]+)_head\/([a-z_]+)/.exec(MainHandItem.id);
+    let ToolMaterial = ToolTypesMatch ? ToolTypesMatch[1] :null;
+    let ToolTypes = ToolTypesMatch ? ToolTypesMatch[2] : null;
+    let ToolMetal = ToolTypesMatch ? ToolTypesMatch[3] : null;
+
+    if (MainHandItem.equals(Item.of(`tfc:${ToolMaterial}/${ToolTypes}_head/${ToolMetal}`,`{Damage:0,"tfc:forging_bonus":${forgingBonus}}`))||
+    (`tfc:${ToolMaterial}/${ToolTypes}_head/${ToolMetal}`) && offHandItem === 'minecraft:stick') {
         if(event.hand != "MAIN_HAND")return;
         event.player.triggerAnimation("tfcre:crafttool");
         event.player.server.scheduleInTicks(20 * 1.21, () => {
             offHandItem.count--;
             MainHandItem.count--;
-            event.player.giveInHand(Item.of(`tfc:metal/axe/copper`, `{Damage:0,"tfc:forging_bonus":${forgingBonus}}`));
+            event.player.giveInHand(Item.of(`tfc:${ToolMaterial}/${ToolTypes}/${ToolMetal}`, `{Damage:0,"tfc:forging_bonus":${forgingBonus}}`));
         });
     }
 })
