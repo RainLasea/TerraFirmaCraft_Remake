9602a97e2b9fc7b9b61ea3d5602c70dad2eaa8cc
RainLasea <1178391318@qq.com>
2025-02-20 23:39:10 +0800

    Update mod and update method

--- overrides/kubejs/server_scripts/TFCRE_veins.js
+++ overrides/kubejs/server_scripts/TFCRE_veins.js
@@ -2,15 +2,15 @@ TFCEvents.worldgenData((event) => {
     event.pipeVein(
         "aluminum_pipe_vein",
         [
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/chalk"],
                 ["tfc:ore/poor_chalk/aluminum", "tfc:ore/normal_chalk/aluminum", "tfc:ore/rich_chalk/aluminum"]
             ),
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/shale"],
                 ["tfc:ore/poor_shale/aluminum", "tfc:ore/normal_shale/aluminum", "tfc:ore/rich_shale/aluminum"]
             ),
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/claystone"],
                 [
                     "tfc:ore/poor_claystone/aluminum",
@@ -18,7 +18,7 @@ TFCEvents.worldgenData((event) => {
                     "tfc:ore/rich_claystone/aluminum",
                 ]
             ),
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/limestone"],
                 [
                     "tfc:ore/poor_limestone/aluminum",
@@ -26,7 +26,7 @@ TFCEvents.worldgenData((event) => {
                     "tfc:ore/rich_limestone/aluminum",
                 ]
             ),
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/conglomerate"],
                 [
                     "tfc:ore/poor_conglomerate/aluminum",
@@ -34,11 +34,11 @@ TFCEvents.worldgenData((event) => {
                     "tfc:ore/rich_conglomerate/aluminum",
                 ]
             ),
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/dolomite"],
                 ["tfc:ore/poor_dolomite/aluminum", "tfc:ore/normal_dolomite/aluminum", "tfc:ore/rich_dolomite/aluminum"]
             ),
-            event.veinReplacement(
+            event.blockToWeightedBlockState(
                 ["tfc:rock/raw/chert"],
                 ["tfc:ore/poor_chert/aluminum", "tfc:ore/normal_chert/aluminum", "tfc:ore/rich_chert/aluminum"]
             ),
